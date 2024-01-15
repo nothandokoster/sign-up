dceec066fa50dd5e1d2d6545c3e8edad1354a190
nothandokoster <nothandokos@gmail.com>
2024-01-14 19:15:57 -0800

    g
    s

--- apps.js
+++ apps.js
@@ -1,40 +1,71 @@
-const formFields = document.querySelector('.form-fields');
-const formInput = document.querySelector('.input-firstName');
-const formInputLastName = document.querySelector('.input-lastName');
-const formInputEmail = document.querySelector('.input-email');
-const formInputPassword = document.querySelector('.password');
+const form = document.getElementById('form');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
-//const message = document.querySelector('.error-message');
-//const messagelastName = document.querySelector('.error-message-lastName');
-//const Btn = document.querySelector('.my-button');
-formFields.addEventListener('submit', onSubmit);
-function onSubmit(e) {
-  if (formInput.value === '' || formInputLastName.value === '' || formInputEmail.value === '' || formInputPassword.value === '') {
-    message.innerHTML = "PLEASE ENTER ALL FIELDS";
-  } else {
-    console.log('success')
-  }
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  validateInputs();
+});
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
 }
-Btn.addEventListener('click', function (event) {
-  event.preventDefault(); // Prevent default form submission
 
- // const enteredfirstName = formInput.value.trim();
- // const enteredlastName = formInputlastName.value.trim();
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
 
-  // Clear previous errors
- // message.textContent = "";
- // messagelastName.textContent = "";
- // formInput.classList.remove("error");
-  //formInputlastName.classList.remove("error");
-
-  // Check for empty fields
- // if (enteredfirstName === "") {
-  //  message.textContent = 'First name cannot be empty';
-  //  formInput.classList.add("error");
-  }
- // if (enteredlastName === "") {
-   // messagelastName.textContent = 'Last name cannot be empty';
-  //  formInputlastName.classList.add("error");
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
   
-);
+};
 
+const isValidEmail = eamail => {
+  const re = /^(([^<>()[\]\\.,;:\s@]+(\.^<>()[\]\\.,;:\s@]+)*)|(".+"))@((\[[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+const validateInputs = () => {
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (firstNameValue === '') {
+    setError(firstName, 'FIRST NAME IS REQURED');
+  
+  } else {
+    setSuccess(firstName);
+  }
+  if (lastNameValue === '') {
+    setError(lastName, 'LAST NAME IS REQURED');
+  
+  } else {
+    setSuccess(lastName);
+  }
+  if (emailValue === '') {
+    setError(email, 'EMAIL IS REQUIRED');
+  } else if(!isValidEmail(emailValue)){
+    setError(email, 'Please provide a valid email')
+    
+  } else {
+    setSuccess(email);
+  }
+  if (passwordValue === '') {
+    setError(password, 'Password is required');
+  } else if (passwordValue, length < 8) {
+    setError(password, 'Password must be at least 8 chararacters');
+
+  } else {
+    setSuccess(password);
+  }
+
+
+
+
+
+};
